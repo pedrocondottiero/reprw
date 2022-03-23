f32c35986c0653149b5f5bb308f53bfb1d78c300
pedro <pedrovilelabenedito22@gmail.com>
2022-03-23 10:53:00 -0300

    mudanca

--- aula5/ex1.php
+++ aula5/ex1.php
@@ -2,7 +2,12 @@
 $n1 = 7;
 $n2 = 8;
 $media = ($n1 + $n2)/2;
-echo "Você foi Aprovado!"
+
+if($media >= 7)
+echo "Você foi aprovado!";
+
+else
+echo "Você foi reprovado!";
 
 // Usar localhost/reprw após abrir o Apache no XAMPP
 ?>
