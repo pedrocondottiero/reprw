7a6ab283aa1efda201949166e0947afcce867698
pedro <pedrovilelabenedito22@gmail.com>
2022-03-30 11:35:45 -0300

    Fazer depois

---
+++ Lista 3/ex1.php
@@ -0,0 +1,6 @@
+<?php
+$palavra = "Ad Hoc in Aeternam Vitae, Benedictus Eius Tuus Ad Astra";
+echo "A variável contém: " .strlen($palavra) ."caracteres" . "<br>";
+$palavra=trim($palavra);
+echo "A variável agora contém: " .strlen($palavra) . "caracteres";
+?>
