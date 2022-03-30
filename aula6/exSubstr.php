db1eb183b79d59be9bc376c1d9a411955ed2220d
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-03-30 10:32:45 -0300

    commit

--- aula6/exSubstr.php
+++ aula6/exSubstr.php
@@ -3,6 +3,6 @@ $data = date(“m-d-Y”);
 $dia = substr($data,3,2);
 $mês = substr($data,0,2);
 $ano = substr($data,6,4);
-$novadata = $dia . “/” . $mês . “/” . $ano;
+$novadata = $dia . "/" . $mês . "/" . $ano;
 echo $novadata;
 ?>
