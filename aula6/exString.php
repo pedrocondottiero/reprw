96d4993fa841b51cc92aeb33d1e9f7d162ebe082
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-03-30 10:13:52 -0300

    commit

--- aula6/exString.php
+++ aula6/exString.php
@@ -3,4 +3,5 @@ $palavra = "Ciência da Computação";
 echo "A variável contém: " .strlen($palavra) ."caracteres" . "<br>";
 $palavra=trim($palavra);
 echo "A variável agora contém: " .strlen($palavra) . "caracteres";
+
 ?>
