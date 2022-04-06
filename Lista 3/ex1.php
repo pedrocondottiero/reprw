9d6ece929dfaa6e2ad6d74331585f564153e5eda
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-04-06 10:53:53 -0300

    EX1

--- Lista 3/ex1.php
+++ Lista 3/ex1.php
@@ -3,4 +3,5 @@ $palavra = "Ad Hoc in Aeternam Vitae, Benedictus Eius Tuus Ad Astra";
 echo "A variável contém: " .strlen($palavra) ."caracteres" . "<br>";
 $palavra=trim($palavra);
 echo "A variável agora contém: " .strlen($palavra) . "caracteres";
+
 ?>
