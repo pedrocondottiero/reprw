56c2f052afcb5189dda4a0ea6ab287d8de411c79
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-04-06 10:08:28 -0300

    EX2

--- Lista 3/ex2.php
+++ Lista 3/ex2.php
@@ -1,7 +1,18 @@
 <?php
-$palavra = "Aeternam";
-echo str_replace('A','X',$palavra);
+$palavra = "Pedro";
 
-$nova_pal
+$nova_pal = str_replace('a', 'X',$palavra);
+$nova_pal = str_replace('e', 'X',$nova_pal);
+$nova_pal = str_replace('i', 'X',$nova_pal);
+$nova_pal = str_replace('o', 'X',$nova_pal);
+$nova_pal = str_replace('u', 'X',$nova_pal);
+
+$nova_pal = str_replace('A', 'X',$nova_pal);
+$nova_pal = str_replace('E', 'X',$nova_pal);
+$nova_pal = str_replace('I', 'X',$nova_pal);
+$nova_pal = str_replace('O', 'X',$nova_pal);
+$nova_pal = str_replace('U', 'X',$nova_pal);
+
+echo $nova_pal;
 
 ?>
