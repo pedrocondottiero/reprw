8bd3a20c2f494e1d517ba307a0322749a20a5457
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-04-06 11:02:35 -0300

    EXEMPLO

--- aula5/ex3.php
+++ aula5/ex3.php
@@ -1,9 +1,10 @@
 <?php
-$n1 = 1;
-$n2 = 2;
-$n3 = 3;
-
-
-
-
+    $op = 'a';
+    switch($op){
+        case 'a': echo "Abrir"; break;
+        case 'f': echo "Fechar"; break;
+        case 's': echo "Salvar"; break;
+        case 'e': echo "Esperar"; break;
+        default: echo "Sair"; break; 
+    }
 ?>
