e6a5d370609a69f673d89b2d3ad24a2effed47d0
Pedro Benedito <102226811+pedrocondottiero@users.noreply.github.com>
2022-04-20 09:45:11 -0300

    Projeto 01

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome_usuario = $_POST['nome'];
+$email_usuario = $_POST['email'];
+$fone_usuario = $_POST['telefone'];
+
+echo "<P>Nome do Usuário: " .$nome_usuario ."<br>";
+echo "<P>E-mail do Usuário: " .$email_usuario ."<br>";
+echo "<P>Telefone do Usuário: " .$fone_usuario ."<br>";
+
+?>
