ca9743273ce79ad73482f1e07b4bb9b952fb020f
Pedro Benedito <102226811+pedrocondottiero@users.noreply.github.com>
2022-04-20 11:21:48 -0300

    Cadastro, Banco de Dados (SQL) e PHP

---
+++ projeto01/dados.sql
@@ -0,0 +1,6 @@
+USE projeto01;
+
+INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+VALUES ('Pedro Benedito', 'pedrovilelabenedito22@gmail.com', '1899103-8486');
+
+SELECT * FROM usuario;
