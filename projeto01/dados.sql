a4fe2192965c51ede8a56057be393b462a17ad22
Pedro Benedito <102226811+pedrocondottiero@users.noreply.github.com>
2022-04-20 11:46:19 -0300

    Delete projeto01 directory

--- projeto01/dados.sql
+++
@@ -1,6 +0,0 @@
-USE projeto01;
-
-INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
-VALUES ('Pedro Benedito', 'pedrovilelabenedito22@gmail.com', '1899103-8486');
-
-SELECT * FROM usuario;
