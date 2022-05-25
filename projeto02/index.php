0b9bf0e020cc49bd72b3da69ce449dce451f5553
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-05-25 10:43:37 -0300

    Atualização

--- projeto02/index.php
+++ projeto02/index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu principal</title>
+    <title>Menu Principal</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    <h1>Menu principal</h1>
+    <h1>Menu Principal</h1>
     <div id="menu">
         <ul>
-            <li><a href="cadastro_usuario.html"> Cadastrar usuário</a></li>
-            <li><a href="listar_usuarios.php"> Listar usuários</a></li>
+            <li><a href="cadastro_usuario.html">Cadastrar Usuários</a></li>
+            <li><a href="listar_usuarios.php">Listar Usuários</a></li>
         </ul>
     </div>
 </body>
