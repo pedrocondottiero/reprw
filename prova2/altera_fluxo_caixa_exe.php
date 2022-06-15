5b1c7b3bc9f23d9f9b8e77cd1c867f59b6dd5968
pedrobenedito <pedrovilelabenedito22@gmail.com>
2022-06-15 10:52:36 -0300

    Correção

--- prova2/altera_fluxo_caixa_exe.php
+++ prova2/altera_fluxo_caixa_exe.php
@@ -1,28 +1,30 @@
-<?php
+<?php 
+    include ('conexao.php');
 
-include("conexao.php");
-$id = $_POST["id"];
-$data = date("Y-m-d");
-$tipo = $_POST['tipo'];
-$valor = $_POST['valor'];
-$historico = $_POST['historico'];
-$cheque = $_POST['cheque'];
-
-echo "<h1> Alteração de Dados </h1>";
-    echo "<p> Tipo: " . $tipo . "<p>";
-
-	$sql = "UPDATE fluxo_caixa SET
-          tipo='".$tipo."',
-          valor='".$valor."',
-          historico='".$historico."',
-          cheque='".$cheque."'
-          WHERE id=.$id";
-
+    $id = $_POST['id'];
+    $data = $_POST['data'];
+    $tipo = $_POST['opt'];
+    $valor = $_POST['valor'];
+    $historico = $_POST['historico'];
+    $cheque = $_POST['cheque'];
+    
+    echo "<h1> Alteração de dados </h1>";
+    echo "<p> ID usuário: " . $id . "<p>";
+    
+    $sql = "UPDATE fluxo_caixa SET
+            data='$data',
+            tipo='$tipo',
+            valor='$valor',
+            historico='$historico',
+            cheque='$cheque'
+        WHERE id=$id";
+	    
 	$result = mysqli_query($con, $sql);
-	if($result)
+	if($result){
 		echo "Dados alterados com sucesso <br>";
-	else
+    }
+	else{
 		echo "Erro ao alterar no banco de dados: ".mysqli_error($con)."<br>";
-  
+    }
 ?>
-<a href='index.php'>Voltar</a>
+<a href='listar_fluxo_caixa.php'>Voltar</a>
